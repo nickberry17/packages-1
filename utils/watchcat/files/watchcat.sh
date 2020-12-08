e7b4cc0aca60b1e6f72cbeecd00b143aeadc86f6
Nicholas Smith <nicholas.smith@telcoantennas.com.au>
2020-12-08 20:55:44 +1000

    watchcat: refactor code and add features - support for monitoring and restarting an interface, support for ping packet sizes

--- utils/watchcat/files/watchcat.sh
+++ utils/watchcat/files/watchcat.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 #
 # Copyright (C) 2010 segal.di.ubi.pt
+# Copyright (C) 2020 nbembedded.com
+# Support for multiple interfaces, ModemManager interfaces, restarting
+# an interface, and ping packet size added by nbembedded.com
 #
 # This is free software, licensed under the GNU General Public License v2.
 #
 
 mode="$1"
 
-# Fix potential typo in mode (backward compatibility).
-[ "$mode" = "allways" ] && mode="always"
+# Fix potential typo in mode and provide backward compatibility.
+[ "$mode" = "allways" ] && mode="periodic_reboot"
+[ "$mode" = "always" ] && mode="periodic_reboot"
+[ "$mode" = "ping" ] && mode="ping_reboot"
 
 get_ping_size() {
 	ps=$1
@@ -32,7 +37,7 @@ get_ping_size() {
 		ps="9000"
 		;;
 	*)
-		echo "Error: invalid pingsize. pingsize should be small, windows, standard, big, huge or jumbo"
+		echo "Error: invalid ping_size. ping_size should be small, windows, standard, big, huge or jumbo"
 		echo "Cooresponding ping packet sizes (bytes): small=1, windows=32, standard=56, big=248, huge=1492, jumbo=9000"
 		;;
 	esac
@@ -40,44 +45,51 @@ get_ping_size() {
 }
 
 shutdown_now() {
-	local forcedelay="$1"
+	local force_delay="$1"
 
 	reboot &
 
-	[ "$forcedelay" -ge 1 ] && {
-		sleep "$forcedelay"
+	[ "$force_delay" -ge 1 ] && {
+		sleep "$force_delay"
 
 		echo b >/proc/sysrq-trigger # Will immediately reboot the system without syncing or unmounting your disks.
 	}
 }
 
-watchcat_always() {
+watchcat_periodic() {
 	local period="$1"
-	local forcedelay="$2"
+	local force_delay="$2"
 
-	sleep "$period" && shutdown_now "$forcedelay"
+	sleep "$period" && shutdown_now "$force_delay"
 }
 
-watchcat_restart_modemmanager() {
-	logger -t INFO "Connection lost. Resetting bands and reconnecting modem now."
+watchcat_restart_modemmanager_iface() {
+	logger -t INFO "Resetting bands and reconnecting modem: $1 now."
 	/usr/bin/mmcli -m any --set-current-bands=any
 	/etc/init.d/modemmanager restart
-	ifup mobiledata
+	ifup $1
 }
 
-watchcat_monitor_modemmanager() {
+watchcat_restart_network_iface() {
+	logger -t INFO "Restarting network interface: $1."
+	ifdown $1
+	ifup $1
+}
+
+watchcat_monitor_network() {
 	local period="$1"
-	local forcedelay="$2"
-	local pinghosts="$3"
-	local pingperiod="$4"
-	local pingsize="$5"
+	local force_delay="$2"
+	local ping_hosts="$3"
+	local ping_period="$4"
+	local ping_size="$5"
+	local wc_interface="$6"
+	local mm_iface_name="$7"
 
 	time_now="$(cat /proc/uptime)"
 	time_now="${time_now%%.*}"
 	time_lastcheck="$time_now"
 	time_lastcheck_withinternet="$time_now"
-
-	pingsize="$(get_ping_size "$pingsize")"
+	ping_size="$(get_ping_size "$ping_size")"
 
 	while true; do
 		# account for the time ping took to return. With a ping time of 5s, ping might take more than that, so it is important to avoid even more delay.
@@ -85,8 +97,8 @@ watchcat_monitor_modemmanager() {
 		time_now="${time_now%%.*}"
 		time_diff="$((time_now - time_lastcheck))"
 
-		[ "$time_diff" -lt "$pingperiod" ] && {
-			sleep_time="$((pingperiod - time_diff))"
+		[ "$time_diff" -lt "$ping_period" ] && {
+			sleep_time="$((ping_period-time_diff))"
 			sleep "$sleep_time"
 		}
 
@@ -94,20 +106,25 @@ watchcat_monitor_modemmanager() {
 		time_now="${time_now%%.*}"
 		time_lastcheck="$time_now"
 
-		for host in $pinghosts
+		for host in $ping_hosts
 		do
-			if ping -c 1 "$host" &> /dev/null
+			if ping -I "$wc_interface" -c 1 "$host" &> /dev/null
 			then
 				time_lastcheck_withinternet="$time_now"
 			else
 				time_diff="$((time_now-time_lastcheck_withinternet))"
-				logger -p daemon.info -t "watchcat[$$]" "no internet connectivity for $time_diff seconds. Restarting modem reaching $period"
+				logger -p daemon.info -t "watchcat[$$]" "no internet connectivity on interface $wc_interface for $time_diff seconds. Restarting $wc_interface after reaching $period"
 			fi
 		done
 
 		time_diff="$((time_now - time_lastcheck_withinternet))"
 		[ "$time_diff" -ge "$period" ] && {
-			watchcat_restart_modemmanager
+			if [ "$mm_iface_name" -ne "" ] 
+			then
+				watchcat_restart_network_iface "$wc_interface"
+			else
+				watchcat_restart_modemmanager_iface "$mm_iface_name"
+			fi
 			/etc/init.d/watchcat start
 		}
 
@@ -116,17 +133,17 @@ watchcat_monitor_modemmanager() {
 
 watchcat_ping() {
 	local period="$1"
-	local forcedelay="$2"
-	local pinghosts="$3"
-	local pingperiod="$4"
-	local pingsize="$5"
+	local force_delay="$2"
+	local ping_hosts="$3"
+	local ping_period="$4"
+	local ping_size="$5"
 
 	time_now="$(cat /proc/uptime)"
 	time_now="${time_now%%.*}"
 	time_lastcheck="$time_now"
 	time_lastcheck_withinternet="$time_now"
 
-	pingsize="$(get_ping_size "$pingsize")"
+	ping_size="$(get_ping_size "$ping_size")"
 
 	while true; do
 		# account for the time ping took to return. With a ping time of 5s, ping might take more than that, so it is important to avoid even more delay.
@@ -134,8 +151,8 @@ watchcat_ping() {
 		time_now="${time_now%%.*}"
 		time_diff="$((time_now - time_lastcheck))"
 
-		[ "$time_diff" -lt "$pingperiod" ] && {
-			sleep_time="$((pingperiod - time_diff))"
+		[ "$time_diff" -lt "$ping_period" ] && {
+			sleep_time="$((ping_period-time_diff))"
 			sleep "$sleep_time"
 		}
 
@@ -143,8 +160,8 @@ watchcat_ping() {
 		time_now="${time_now%%.*}"
 		time_lastcheck="$time_now"
 
-		for host in $pinghosts; do
-			if ping -s "$pingsize" -c 1 "$host" &>/dev/null; then
+		for host in $ping_hosts; do
+			if ping -s "$ping_size" -c 1 "$host" &>/dev/null; then
 				time_lastcheck_withinternet="$time_now"
 			else
 				time_diff="$((time_now - time_lastcheck_withinternet))"
@@ -153,20 +170,20 @@ watchcat_ping() {
 		done
 
 		time_diff="$((time_now - time_lastcheck_withinternet))"
-		[ "$time_diff" -ge "$period" ] && shutdown_now "$forcedelay"
+		[ "$time_diff" -ge "$period" ] && shutdown_now "$force_delay"
 
 	done
 }
 
 case "$mode" in
-always)
-	watchcat_always "$2" "$3"
+periodic_reboot)
+	watchcat_periodic "$2" "$3"
 	;;
-ping)
+ping_reboot)
 	watchcat_ping "$2" "$3" "$4" "$5" "$6"
 	;;
-network)
-	watchcat_monitor_modemmanager "$2" "$3" "$4" "$5" "$6"
+restart_iface)
+	watchcat_monitor_network "$2" "$3" "$4" "$5" "$6" "$7"
 	;;
 *)
 	echo "Error: invalid mode selected: $mode"
